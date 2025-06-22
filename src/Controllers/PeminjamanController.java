16e2ac8f6a49855a90bc9be7eef48a28782d1344
Juan <juwanyafi9@gmail.com>
2025-06-22 20:06:17 +0700

    menambahkan function logic contoller di class peminjaman controller

--- src/Controllers/PeminjamanController.java
+++ src/Controllers/PeminjamanController.java
@@ -1,6 +1,30 @@
 package Controllers;
 
-//Ini class yang handle logika Peminjaman
-public class PeminjamanController {
+import Models.Peminjaman;
+import Models.PenyimpananData;
+import java.util.Scanner;
 
+// CLASS PEMINJAMANCONTROLLER UNTUK MENGELOLA TRANSAKSI PEMINJAMAN DAN PENGEMBALIAN
+public class PeminjamanController {
+    // ATTRIBUTE
+    private PenyimpananData data;
+    private Scanner scanner = new Scanner(System.in);
+
+    // CONSTRUCTOR
+    public PeminjamanController(PenyimpananData data) {
+        this.data = data;
+    }
+
+    // METHOD TAMBAH PEMINJAMAN
+    public void tambahPeminjaman(Peminjaman peminjaman) {
+        data.getDataPeminjaman().add(peminjaman);
+        System.out.println("PEMINJAMAN BERHASIL DITAMBAHKAN");
+    }
+
+    // METHOD TAMPILKAN PEMINJAMAN
+    public void tampilkanPeminjaman() {
+        for (Peminjaman p : data.getDataPeminjaman()) {
+            System.out.println(p.generateReport());
+        }
+    }
 }
